fa69628edf5a099055f64c4be1d71dd50da6b5bb
James Spears <jamesspearsv@gmail.com>
2024-12-24 23:10:49 -0500

    Add and style home page search box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,77 @@
-import * as data from "../data.json";
 import CountryCard from "../components/CountryCard/CountryCard.tsx";
 import { Country } from "../types/country";
-
-//https://restcountries.com/v3.1/all?fields=name,population,region,subregion,capital,cca3,borders,flags,tld,currencies,languages
-
-// @ts-expect-error -> importing development data
-const countries = data.default;
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Home.module.css";
 
 export default function HomePage() {
-  console.log(countries[200]);
+  const [countries, setCountries] = useState<Country[] | null>(null);
+  const [error, setError] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // todo add ability to search and filter api data
+  useEffect(() => {
+    // fetch data from API
+    (async () => {
+      const fields =
+        "?fields=name,population,region,subregion,capital,cca3,borders,flags,tld,currencies,languages";
+      try {
+        const request = {
+          all: "https://restcountries.com/v3.1/all",
+          search: `https://restcountries.com/v3.1/name/${searchTerm}`,
+        };
+
+        const res = await fetch(request.all + fields);
+        if (!res.ok) throw new Error("Something went wrong");
+
+        const json = await res.json();
+        console.log(json);
+        setCountries(json);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      }
+    })();
+
+    // clean up effect
+    return () => {
+      setCountries(null);
+      setError(false);
+    };
+  }, []);
+
+  if (error) {
+    return (
+      <div style={{ width: "100%", padding: "5rem", textAlign: "center" }}>
+        <p>Error!</p>
+      </div>
+    );
+  }
+
+  if (!countries) {
+    return (
+      <div style={{ width: "100%", padding: "5rem", textAlign: "center" }}>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
-    <div style={{ margin: "2rem 6rem" }}>
-      <div
-        style={{
-          display: "grid",
-          gap: "4rem",
-          gridTemplateColumns: "repeat(4, 1fr)",
-        }}
-      >
-        {countries.map((country: Country, index: number) => (
-          <CountryCard key={index} country={country} />
-        ))}
+    <div className={styles.container}>
+      <div className={styles.actionBar}>
+        <div className={styles.searchBox}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+          <input type="text" placeholder="Search for a country..." />
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div className={styles.countryGrid}>
+          {countries.map((country: Country, index: number) => (
+            <CountryCard key={index} country={country} />
+          ))}
+        </div>
       </div>
     </div>
   );
